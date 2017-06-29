f11ca0b5568fa1bd41f39abc16553324957599be
Mike Cooper <mc14641@bris.ac.uk>
2017-06-29 09:55:10 +0100

    Added objects to follow camera

---
+++ Assets/FollowCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour {
+
+	public Camera MainCamera;
+	public Transform Object;
+
+	void Start() {
+//		Object.transform.position.Set (MainCamera.transform.position.x, MainCamera.transform.position.y, MainCamera.transform.position.z);
+		// Tie this to the camera, and do not keep the local orientation.
+		Object.transform.SetParent(MainCamera.GetComponent<Transform>(), true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
